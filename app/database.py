5815ae519fc6eeca2a887c80ba8bfe4cd86be2bc
DorianQA-WEB <qa.doriangrey@gmail.com>
2025-12-22 23:12:27 +0500

    Docker

--- app/database.py
+++ app/database.py
@@ -20,7 +20,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import DeclarativeBase
 
 # Строка подключения для PostgreSQl
-DATABASE_URL = "postgresql+asyncpg://ecommerce_user:qwerty@localhost:5432/ecommerce_db"
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:qwerty@db:5432/ecommerce_db"
 
 # Создаём Engine
 async_engine = create_async_engine(DATABASE_URL, echo=True)
